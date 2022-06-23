b568f559626a5688d42bf0c2e9f398b6235b87c5
Muhammad Irfan <irfhan025@gmail.com>
2022-06-23 16:27:30 +0800

    update

--- components/BodySection.js
+++ components/BodySection.js
@@ -174,9 +174,15 @@ const Album = styled.div`
 const Wrapper = styled.div`
   display: grid;
   grid-gap: 30px;
-  grid-template-columns: repeat(2, minmax(0, 1fr));
+  grid-template-columns: repeat(4, minmax(0, 1fr));
+  padding: 50px;
+
+  @media (max-width: 1200px) {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
 
   @media (max-width: 768px) {
     grid-template-columns: repeat(1, minmax(0, 1fr));
+    padding: 20px 14vw;
   }
 `;
